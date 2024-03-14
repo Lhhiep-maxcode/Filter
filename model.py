87a1c54916cd84913d702159efe3d2154953287c
Lhhiep-maxcode <122993600+Lhhiep-maxcode@users.noreply.github.com>
2024-03-14 21:09:25 +0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+architecture_config = [
+    # Tuple: (square size, filters, stride, padding)
+    (7, 64, 2, 3),
+    # M: Maxpool 2x2, stride = 2
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    # List: [tuple,..., tuple, numOfRepeat]
+    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+]
+
+
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.leakyrelu = nn.LeakyReLU(0.1)
+
+    
